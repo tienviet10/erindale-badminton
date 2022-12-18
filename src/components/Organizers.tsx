61452cfdb63d671f6c91136d5fc3a00618711038
Viet Tran <viettran101294@gmail.com>
2022-12-18 18:07:02 -0500

    add Izzy pictures

--- src/components/Organizers.tsx
+++ src/components/Organizers.tsx
@@ -1,23 +1,23 @@
 import andrew1 from "../assets/andrew1.webp";
 import andrew2 from "../assets/andrew2.webp";
+import izzy1 from "../assets/izzy1.webp";
+import izzy2 from "../assets/izzy2.webp";
 import tris2 from "../assets/Tris.webp";
 import tris1 from "../assets/Tris1.webp";
 import viet1 from "../assets/viet1.webp";
 import viet2 from "../assets/viet2.webp";
 import viet3 from "../assets/viet3.webp";
-import viet4 from "../assets/viet4.webp";
 import viet5 from "../assets/viet5.webp";
-import person2 from "../assets/viet_badminton.webp";
 import ImageIndividual from "./ImageIndividual";
 
 const namePositionMap1 = {
-  "Sara Gautier": ["President", viet4, viet1],
+  "Sara Gautier": ["President", viet1, viet3, ],
   "Michael Ho": ["Vice President", viet5, viet2],
 
 }
 
 const namePositionMap2 = {
-  "Izabela Ligieza": ["Secretary", viet3, person2],
+  "Izabela Ligieza": ["Secretary", izzy2, izzy1],
   "Tristan Wu": ["Treasurer", tris1, tris2],
   "Andrew Lye": ["Equipment Manager", andrew1, andrew2]
 }
