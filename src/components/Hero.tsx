d1ce65b0fa64f366008a7ce8da4b138389571abd
Viet Tran <viettran101294@gmail.com>
2022-12-18 22:27:31 -0500

    responsive bug fixes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,11 +8,11 @@ const Hero = () => {
       <div className="bg-[#1c1854] text-white py-20">
         <div className="container mx-auto flex flex-col lg:flex-row items-center my-12 lg:my-24">
           <div className="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
-            <h1 className="text-2xl xl:text-4xl pt-3 pb-3 tracking-loose text-yellow-300">Welcome!</h1>
-            <h2 className="text-3xl  xl:text-5xl leading-relaxed xl:leading-snug mb-3 ">Erindale Badminton
+            <h1 className="text-2xl xl:text-3xl 2xl:text-4xl pt-3 pb-3 tracking-loose text-yellow-300">Welcome!</h1>
+            <h2 className="text-3xl xl:text-4xl 2xl:text-5xl leading-relaxed xl:leading-snug mb-3 ">Erindale Badminton
             </h2>
             <p className="text-sm lg:text-base text-gray-50 mb-4 text-justify">Erindale Badminton Club is a recreational badminton club for adults 18 years of age and older. The club aims to provide its 90 members fair and equitable access to our facilities, regardless of each member's level of play.</p>
-            <div className="mt-4 lg:mt-2 z-10">
+            <div className="mt-4 lg:mt-4 z-10">
               <a href="#Information"
                 className="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent">
                 More Info
