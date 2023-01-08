3f9cbb5182e21a1544ad80c4ae6a27b9c1aab99d
Viet Tran <viettran101294@gmail.com>
2023-01-08 10:08:33 -0500

    minor changes v2

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -7,6 +7,52 @@ const Pricing = () => {
 
         <div className="w-full flex flex-col lg:flex-row space-x-0 lg:space-x-20 justify-center items-center lg:items-stretch py-10">
           <div className="mb-6 w-[100%] lg:mb-0 max-w-[400px] lg:w-[30%] text-lg">
+              <div className="block rounded-lg shadow-lg bg-gray-100 h-full">
+                <div className="p-6 border-b border-gray-300 text-center">
+                  <p className="uppercase mb-4 text-xl">
+                    <strong>Guest</strong>
+                  </p>
+                  <h3 className="text-2xl mb-6">
+                    <strong>$ 5</strong>
+                    <small className="text-gray-900 text-md">/drop-in</small>
+                  </h3>
+                </div>
+                <div className="p-6">
+                  <ol className="list-inside">
+                    <li className="mb-4 flex items-center">
+                      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check"
+                        className="text-green-600 w-4 h-4 mr-2 shrink-0" xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512">
+                        <path fill="currentColor"
+                          d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z">
+                        </path>
+                      </svg>Play a 2-hour session
+                    </li>
+                    <li className="mb-4 flex items-center">
+                      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check"
+                        className="text-green-600 w-4 h-4 mr-2 shrink-0" xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512">
+                        <path fill="currentColor"
+                          d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z">
+                        </path>
+                      </svg>Only available on Wednesday or Friday
+                    </li>
+                    <li className="mb-4 flex items-center">
+                      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check"
+                        className="text-green-600 w-4 h-4 mr-2 shrink-0" xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512">
+                        <path fill="currentColor"
+                          d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z">
+                        </path>
+                      </svg>Limit capacity of total of 10 guests/session
+                    </li>
+        
+                  </ol>
+                </div>
+              </div>
+          </div>
+
+          <div className="mb-6 w-[100%] lg:mb-0 max-w-[400px] lg:w-[30%] text-lg">
             <div className="block rounded-lg shadow-lg bg-gray-100 h-full">
               <div className="p-6 border-b border-gray-300 text-center">
                 <p className="uppercase mb-4 text-xl">
@@ -117,51 +163,7 @@ const Pricing = () => {
             </div>
           </div>
 
-          <div className="mb-6 w-[100%] lg:mb-0 max-w-[400px] lg:w-[30%] text-lg">
-            <div className="block rounded-lg shadow-lg bg-gray-100 h-full">
-              <div className="p-6 border-b border-gray-300 text-center">
-                <p className="uppercase mb-4 text-xl">
-                  <strong>Guest</strong>
-                </p>
-                <h3 className="text-2xl mb-6">
-                  <strong>$ 5</strong>
-                  <small className="text-gray-900 text-md">/drop-in</small>
-                </h3>
-              </div>
-              <div className="p-6">
-                <ol className="list-inside">
-                  <li className="mb-4 flex items-center">
-                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check"
-                      className="text-green-600 w-4 h-4 mr-2 shrink-0" xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 512 512">
-                      <path fill="currentColor"
-                        d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z">
-                      </path>
-                    </svg>Play a 2-hour session
-                  </li>
-                  <li className="mb-4 flex items-center">
-                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check"
-                      className="text-green-600 w-4 h-4 mr-2 shrink-0" xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 512 512">
-                      <path fill="currentColor"
-                        d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z">
-                      </path>
-                    </svg>Only available on Wednesday or Friday
-                  </li>
-                  <li className="mb-4 flex items-center">
-                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check"
-                      className="text-green-600 w-4 h-4 mr-2 shrink-0" xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 512 512">
-                      <path fill="currentColor"
-                        d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z">
-                      </path>
-                    </svg>Limit capacity of total of 10 guests/session
-                  </li>
-      
-                </ol>
-              </div>
-            </div>
-          </div>
+
         </div>
 
         <h2 className="text-3xl lg:text-4xl font-bold text-center text-text-application-color">How to join:</h2>
