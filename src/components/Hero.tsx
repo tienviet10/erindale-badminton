cf51104fc64cd19e0a89f07b980ba5cf97d91163
Viet Tran <viettran101294@gmail.com>
2023-01-14 17:30:59 -0500

    buttons pricing and more info fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,18 +7,18 @@ const Hero = () => {
     <section>
       <div className="bg-application-color text-white py-20">
         <div className="container mx-auto flex flex-col lg:flex-row items-center my-12 lg:my-24 pt-3">
-          <div className="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
+          <div className="flex flex-col w-full lg:w-1/3 justify-center items-start p-8 px-5">
             <h1 className="text-2xl xl:text-3xl 2xl:text-4xl pt-3 pb-3 tracking-loose text-text-application-color">Welcome!</h1>
             <h2 className="text-3xl xl:text-4xl 2xl:text-5xl leading-relaxed xl:leading-snug mb-4 ">Erindale Badminton
             </h2>
             <p className="text-xl lg:text-base text-gray-50 mb-4 text-justify">Erindale Badminton Club is a 50-year strong non-profit badminton club for adults 18 years of age and older. The mission of Erindale Badminton Club is to offer venues where members and their registered guests are afforded an opportunity to play badminton, get exercise and socialize in a respectful and hospitable environment.</p>
             <div className="mt-4 z-10">
               <a href="#Locations"
-                className="bg-transparent hover:bg-text-application-color text-text-application-color hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-text-application-color hover:border-transparent">
+                className="bg-transparent hover:bg-text-application-color text-text-application-color hover:text-black rounded shadow hover:shadow-lg py-2 px-2 sm:px-4 border border-text-application-color hover:border-transparent">
                 More Info
               </a>
               <a href="#Pricing"
-                className="bg-transparent hover:bg-text-application-color text-text-application-color hover:text-black rounded shadow hover:shadow-lg py-2 px-6 border border-text-application-color hover:border-transparent ml-4">
+                className="bg-transparent hover:bg-text-application-color text-text-application-color hover:text-black rounded shadow hover:shadow-lg py-2 px-4 sm:px-6 border border-text-application-color hover:border-transparent ml-4">
                 Pricing
               </a>
             </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
         "application-color": "var(--application-color)",
