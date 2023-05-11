eb1780fd9281f4de3f84b7807381ecf23a31a3fa
Viet Tran <viettran101294@gmail.com>
2023-05-10 20:40:15 -0400

    Change announcement banner

--- src/components/AnnouncementBanner.tsx
+++ src/components/AnnouncementBanner.tsx
@@ -1,21 +1,37 @@
-
-const AnnouncementBanner = ({setOpenedModal}) => {
+const AnnouncementBanner = ({ setOpenedModal }) => {
   return (
     <div className="bg-yellow-400">
       <div className="mx-auto max-w-7xl py-2 px-3 sm:px-6 lg:px-8">
         <div className="flex flex-row items-center justify-between">
           <div className="flex w-[70%] sm:w-[80%]">
             <span className="rounded-lg bg-yellow-500 p-2 border border-red-800">
-              <svg className="h-4 w-4 text-red-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46" />
+              <svg
+                className="h-4 w-4 text-red-900"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46"
+                />
               </svg>
             </span>
             <p className="w-[90%] ml-3 truncate font-medium text-black my-auto text-semibold">
-              Cancellations: Wed. Apr. 26 and Wed. May 10.
+              Cancellations: Wed. May. 24.
             </p>
           </div>
           <div className="w-full flex sm:mt-0 sm:w-auto justify-center">
-            <button className="flex items-center justify-center rounded-md border border-transparent bg-red-800 px-4 py-1 text-sm font-medium text-white shadow-sm" onClick={()=>setOpenedModal(true)}>Details</button>
+            <button
+              className="flex items-center justify-center rounded-md border border-transparent bg-red-800 px-4 py-1 text-sm font-medium text-white shadow-sm"
+              onClick={() => setOpenedModal(true)}
+            >
+              Details
+            </button>
           </div>
           {/* <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
             <button type="button" className="-mr-1 flex rounded-md p-2 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
@@ -29,7 +45,7 @@ const AnnouncementBanner = ({setOpenedModal}) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AnnouncementBanner
+export default AnnouncementBanner;
--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,54 +1,56 @@
-
-const Details = ({setOpenedModal}) => {
+const Details = ({ setOpenedModal }) => {
   return (
     <div
-    id="defaultModal"
-    className="fixed top-0 left-0 right-0 w-full p-4 md:inset-0 h-modal md:h-full z-200 bg-gray-600 bg-opacity-50"
-  >
-    <div className="relative mx-auto w-full h-screen max-w-2xl flex flex-col justify-center items-center">
-      <div className="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
-        <div className="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
-          <h5
-            className="text-xl font-medium leading-normal text-gray-800"
-            id="exampleModalScrollableLabel"
-          >
-            Erindale Badminton Club
-          </h5>
-          <button className="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline" onClick={()=> setOpenedModal(false)}>
-            <svg
-              className="h-6 w-6 text-black"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              aria-hidden="true"
+      id="defaultModal"
+      className="fixed top-0 left-0 right-0 w-full p-4 md:inset-0 h-modal md:h-full z-200 bg-gray-600 bg-opacity-50"
+    >
+      <div className="relative mx-auto w-full h-screen max-w-2xl flex flex-col justify-center items-center">
+        <div className="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+          <div className="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
+            <h5
+              className="text-xl font-medium leading-normal text-gray-800"
+              id="exampleModalScrollableLabel"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-        </div>
-        <div className="modal-body relative p-4">
-          <p>Cancellations: Wed. Apr. 26 and Wed. May 10.</p>
-        </div>
-        <div className="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
-          <button
-            type="button"
-            className="inline-block px-6 py-2.5 bg-red-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md"
-            data-bs-dismiss="modal"
-            onClick={()=> setOpenedModal(false)}
-          >
-            Close
-          </button>
+              Erindale Badminton Club
+            </h5>
+            <button
+              className="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+              onClick={() => setOpenedModal(false)}
+            >
+              <svg
+                className="h-6 w-6 text-black"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="modal-body relative p-4">
+            <p>Cancellations: Wed. May. 24</p>
+          </div>
+          <div className="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
+            <button
+              type="button"
+              className="inline-block px-6 py-2.5 bg-red-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md"
+              data-bs-dismiss="modal"
+              onClick={() => setOpenedModal(false)}
+            >
+              Close
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Details
+export default Details;
