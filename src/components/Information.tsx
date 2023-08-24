8351af01c6e3a0d0d504a3c741ebc7d4fbeca551
tienviet10 <viettran101294@gmail.com>
2023-08-24 17:43:52 -0400

    Removed Clarkson HS

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -91,7 +91,7 @@ const Information = () => {
           <p className="mb-2 font-light text-md">
             {/* * Guests are welcome to join on Wednesday and Friday. Consult
             Pricing section for the fees. Contact us to reserve your guest spot. */}
-            Guests are invited only.
+            * Guests are invited only.
           </p>
         </div>
       </div>
