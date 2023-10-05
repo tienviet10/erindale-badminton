94a179398fac0788293f4fc6271463f4bd8b98b1
tienviet10 <viettran101294@gmail.com>
2023-10-05 18:55:09 -0400

    Change full season fees

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -182,8 +182,8 @@ const Pricing = () => {
                   <strong>Full Season</strong>
                 </p>
                 <h3 className="text-2xl mb-6">
-                  <strong>$ 170</strong>
-                  <small className="text-gray-900 text-md">/year</small>
+                  <strong>N/A</strong>
+                  {/* <small className="text-gray-900 text-md">/year</small> */}
                 </h3>
                 {/* <button type="button"
                   className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
