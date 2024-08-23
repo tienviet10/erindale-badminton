fbcf76eb480fd23b4941ed9690cebb45a90f636e
gohfreddy314 <flgoh@uwaterloo.ca>
2024-08-22 20:49:38 -0400

    Guest fee increase and days

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -19,7 +19,7 @@ const Pricing = () => {
                   <strong>Guest</strong>
                 </p>
                 <h3 className="text-2xl mb-6">
-                  <strong>$ 5</strong>
+                  <strong>$ 10</strong>
                   <small className="text-gray-900 text-md">/drop-in</small>
                 </h3>
               </div>
@@ -57,7 +57,7 @@ const Pricing = () => {
                         d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"
                       ></path>
                     </svg>
-                    Only available on Wednesday or Friday
+                    Available on Monday, Wednesday, and Friday
                   </li>
                   <li className="mb-4 flex items-center">
                     <svg
