f560413f69ffe9a7e8de1114312614cf53fb5f8b
gohfreddy314 <flgoh@uwaterloo.ca>
2024-12-19 19:51:33 -0500

    Update AnnouncementBanner.tsx

--- src/components/AnnouncementBanner.tsx
+++ src/components/AnnouncementBanner.tsx
@@ -6,7 +6,7 @@ const AnnouncementBanner = ({ setOpenedModal }) => {
       <div className="mx-auto max-w-7xl py-2 px-3 sm:px-6 lg:px-8">
         <div className="flex flex-row items-center justify-between">
           {/* <div className="flex w-[70%] sm:w-[80%]">*/}
-            <div className = flex-shrink-0>
+            <div className = "flex-shrink-0">
               <span className="rounded-lg bg-yellow-500 p-2 border border-red-800 h-8">
                 <svg
                   className="h-4 w-4 text-red-900"
@@ -37,15 +37,7 @@ const AnnouncementBanner = ({ setOpenedModal }) => {
               Details
             </button>
           </div>
-          {/* <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
-            <button type="button" className="-mr-1 flex rounded-md p-2 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
-              <span className="sr-only">Dismiss</span>
-
-              <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </button>
-          </div> */}
+          
         </div>
       </div>
     </div>
