83253f6d25858f04f196f22ed97e3279599410c6
gohfreddy314 <flgoh@uwaterloo.ca>
2025-01-28 08:05:32 -0500

    Update AnnouncementBanner.tsx

--- src/components/AnnouncementBanner.tsx
+++ src/components/AnnouncementBanner.tsx
@@ -24,7 +24,7 @@ const AnnouncementBanner = ({ setOpenedModal }) => {
                 </svg>
               </span>
             <p className="w-[90%] ml-3 truncate font-medium text-black my-auto text-semibold">
-              Wed. Jan 8 cancelled! We will resume Fri. Jan 10 instead. 
+              No session Mon. Feb 17 for family day and Wed. Feb 26 due to cancellation.
             </p>
           </div>
 
