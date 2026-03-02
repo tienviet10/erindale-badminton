5e3e4440cc13827015538a137a633fbcd42c1396
gohfreddy314 <flgoh@uwaterloo.ca>
2026-03-01 22:35:58 -0500

    Update Details.tsx

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -44,6 +44,9 @@ const Details = ({ setOpenedModal, from }) => {
                 <br></br>
                 <p>Holidays and cancellations: </p>
                 <ul>
+                  <li>Friday, Mar 20, 2026 (March Break)</li>
+                  <li>Wednesday, Mar 18, 2026 (March Break)</li>
+                  <li>Monday, Mar 16, 2026 (March Break)</li>
                   <li>Wednesday, Feb 18, 2026 </li>
                   <li>Monday, Feb 16, 2026 (Family Day)</li>
                   <li>Monday, Jan 26, 2026</li>
